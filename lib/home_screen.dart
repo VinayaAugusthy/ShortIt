2be1e682733576a89348e2ba885abe7de67c81f0
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-10-31 06:15:11 +0530

    ui design completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,8 +8,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final urlController = TextEditingController();
+  String? _shortenedUrl;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    Size size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'ShortIt',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(
+          left: size.width * 0.1,
+          right: size.width * 0.1,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: urlController,
+              decoration: InputDecoration(
+                labelText: 'Enter yoour URL here',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text('Short URL')),
+            Row(
+              children: [
+                Text(
+                  'Shortened Url : $_shortenedUrl',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.copy,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         primarySwatch: Colors.teal,
         buttonTheme: const ButtonThemeData(buttonColor: Colors.teal),
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.teal),
         useMaterial3: true,
       ),
       home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
