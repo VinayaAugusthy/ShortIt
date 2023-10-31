d413fde723b9908775fae7237ad8f9c1a252fa50
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-10-31 12:16:11 +0530

    correction

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:short_it/home_screen.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
